9b5fac46c869ca9a8d5cd5bfe635198697cb991a
Salama Malek <116031625+Salama-Malek@users.noreply.github.com>
2025-10-05 21:31:48 +0300

    Enhance testimonial slider with review gallery

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,18 +1,68 @@
-import React, { useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
 import SectionHeading from './SectionHeading';
 import Slider from 'react-slick';
 import Ratings from './Ratings';
 import { Icon } from '@iconify/react';
 import { useTranslation } from 'react-i18next';
 
+const REVIEW_IMAGE_FILES = [
+  '11047a5cb70a289ffce3c0e6561d9409.png',
+  '12a3924ef2b896cfba174f30bfc0753e.png',
+  '2689ec40e809b37ec730a5630d44a648.png',
+  '3fefc0d7a020a8d149bd13a338a72277.png',
+  '43d8461bce3214bf3d3a830beb4e4825.png',
+  '461adbea90978a20d803b66ecc2df621.png',
+  '56ad2ac9c8e3762e5e9d83f2887f5a24.png',
+  '59b25a5867817b1a30332046c22f95f7.png',
+  '792d62d464fe2b06692e315f436fab55.png',
+  '79efb6178015069234f034d49eededf6.png',
+  '7b0ea8871f4704ecf8b2482a00d2f7fb.png',
+  '7c658841ab2b0afb39792ad9a1df3154.png',
+  'ae6b13c06b69f4552230f0d7bfadbd8b.png',
+  'c372213c22dc456333c06ee0f8e26c3b.png',
+  'c6ad1c67c637c480134fe1c4f92a4180.png',
+  'e43d41bc435f909ddd2a2dee34944bed.png',
+  'e6c53fcb3f47a75c178bc78270b51d9f.png',
+  'f0a614ad28a990a7fbe8629cebedcc85.png',
+  'f96f0bba13c1c5af6c10e6f49d14c4ed.png',
+];
+
 export default function Testimonial({ data = {} }) {
-const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
   const isRTL = i18n.dir() === 'rtl';
 
   const { sectionHeading = {}, allTestimonial = [] } = data;
   const sliderRef = useRef(null);
 
-  if (!data || !sectionHeading || allTestimonial.length === 0) {
+  const reviewImages = useMemo(
+    () =>
+      REVIEW_IMAGE_FILES.map(
+        (fileName) => `${process.env.PUBLIC_URL || ''}/reviews/${fileName}`
+      ),
+    []
+  );
+
+  const slides = useMemo(() => {
+    if (reviewImages.length === 0) {
+      return [];
+    }
+
+    const hasTestimonials = allTestimonial.length > 0;
+
+    return reviewImages.map((image, index) => {
+      const testimonial = hasTestimonials
+        ? allTestimonial[index % allTestimonial.length]
+        : null;
+      return {
+        image,
+        testimonial,
+        altText:
+          testimonial?.avatarName || testimonial?.avatarCompany || `Client review ${index + 1}`,
+      };
+    });
+  }, [reviewImages, allTestimonial]);
+
+  if (!data || !sectionHeading || slides.length === 0) {
     return null;
   }
 
@@ -21,10 +71,12 @@ const { t, i18n } = useTranslation();
     arrows: false,
     infinite: true,
     autoplay: true,
+    pauseOnHover: true,
     autoplaySpeed: 5000,
     speed: 800,
     slidesToShow: 1,
     slidesToScroll: 1,
+    adaptiveHeight: true,
   };
 
   return (
@@ -34,21 +86,31 @@ const { t, i18n } = useTranslation();
         <div className="testimonial-slider-wrapper" data-aos="fade-up" data-aos-duration="800">
           <div className="testimonial-slider">
             <Slider {...settings} ref={sliderRef}>
-              {allTestimonial.map((item, index) => (
-                <div className="testimonial-card" key={index}>
-                  <div className="testimonial-icon">
-                    <Icon icon="bi:quote" />
-                  </div>
-                  <p className="testimonial-text">{item.reviewText}</p>
-                  <div className="testimonial-author">
-                    <div className="author-img">
-                      <img src={item.avatarImg} alt={item.avatarName} />
+              {slides.map(({ image, testimonial, altText }, index) => (
+                <div className="testimonial-card testimonial-card--enhanced" key={index}>
+                  <div className="testimonial-media">
+                    <div className="testimonial-image-wrapper" data-aos="zoom-in" data-aos-duration="900">
+                      <span className="testimonial-image-overlay" />
+                      <img className="testimonial-image" src={image} alt={altText} loading="lazy" />
                     </div>
-                    <div className="author-info">
-                      <h6 className="author-name">{item.avatarName}</h6>
-                      <span className="author-company">{item.avatarCompany}</span>
-                      <Ratings rating={item.rating} />
-                    </div>
+                    {testimonial && (
+                      <div className="testimonial-content">
+                        <div className="testimonial-icon">
+                          <Icon icon="bi:quote" />
+                        </div>
+                        <p className="testimonial-text">{testimonial.reviewText}</p>
+                        <div className="testimonial-author">
+                          <div className="author-img">
+                            <img src={testimonial.avatarImg} alt={testimonial.avatarName} loading="lazy" />
+                          </div>
+                          <div className="author-info">
+                            <h6 className="author-name">{testimonial.avatarName}</h6>
+                            <span className="author-company">{testimonial.avatarCompany}</span>
+                            <Ratings rating={testimonial.rating} />
+                          </div>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
               ))}
@@ -56,12 +118,10 @@ const { t, i18n } = useTranslation();
           </div>
           <div className="slider-nav">
             <button className="nav-btn prev" onClick={() => sliderRef.current?.slickPrev()}>
-            <Icon icon={isRTL ? 'bi:arrow-right' : 'bi:arrow-left'} />
-
+              <Icon icon={isRTL ? 'bi:arrow-right' : 'bi:arrow-left'} />
             </button>
             <button className="nav-btn next" onClick={() => sliderRef.current?.slickNext()}>
-            <Icon icon={isRTL ? 'bi:arrow-left' : 'bi:arrow-right'} />
-
+              <Icon icon={isRTL ? 'bi:arrow-left' : 'bi:arrow-right'} />
             </button>
           </div>
         </div>
